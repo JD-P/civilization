ead310c2b5584250f602940de82c501dea117d58
John David Pressman <jdpostage@linuxmail.org>
2017-04-20 00:07:06 -0700

    Add admin interface items from models

--- civilization/civforum/admin.py
+++ civilization/civforum/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Board, PublicBoard, Purpose, Tag, RTag, Rigor, Mood, Ban, BanType
 
 # Register your models here.
+
+admin.site.register(Board)
+admin.site.register(PublicBoard)
+admin.site.register(Purpose)
+admin.site.register(Tag)
+admin.site.register(RTag)
+admin.site.register(Rigor)
+admin.site.register(Mood)
+admin.site.register(Ban)
+admin.site.register(BanType)
