1092d2ab7f2b7be0a8561985a1cd16b502e5fc21
John David Pressman <jd@jdpressman.com>
2017-04-24 16:11:02 -0700

    Add names to board-related url patterns

--- civilization/civforum/urls.py
+++ civilization/civforum/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('boards/$', views.boards),
-    url(r'boards/(?P<board_id>[0-9]+)/$', views.board),
+    url('boards/$', views.boards, name="boards"),
+    url(r'boards/(?P<board_id>[0-9]+)/$', views.board, name="board"),
     url('tracker/', views.tracker),
 ]
