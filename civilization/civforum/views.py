a20cdb9c8a3f3dcc85516cc243535c4dc6542a8f
John David Pressman <jd@jdpressman.com>
2017-04-24 14:55:33 -0700

    Add individual board view

--- civilization/civforum/views.py
+++ civilization/civforum/views.py
@@ -12,6 +12,14 @@ def boards(request):
     else:
         HttpResponse("POST requests not accepted on this view.")
 
+def board(request, board_id=0):
+    if request.method == 'GET':
+        pboard = PublicBoard.objects.filter(board__id=board_id)[0]
+        threads = Thread.objects.filter(board=pboard.board)
+        return render(request,
+                      'board.html',
+                      {'threads':threads})
+        
 def tracker(request):
     if request.method == 'GET':
         return render(request,
