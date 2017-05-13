a2472c182ddb299820c7662b62795a7131cfbbf9
John David Pressman <jdpostage@linuxmail.org>
2017-05-12 18:32:02 -0700

    Add dirty hack to stop errors when setting up from repository

--- civilization/civforum/forms.py
+++ civilization/civforum/forms.py
@@ -1,13 +1,19 @@
 from django import forms
 from civforum.models import *
 
+from django.db.utils import OperationalError
+#TODO:Fix this dirty dirty hack
+try:
+    boards = [(board.board.id, board.board.title) for board in
+              PublicBoard.objects.all()]
+    
+    rigors = [(rigor.id, rigor.label) for rigor in Rigor.objects.all()]
 
-boards = [(board.board.id, board.board.title) for board in
-          PublicBoard.objects.all()]
-
-rigors = [(rigor.id, rigor.label) for rigor in Rigor.objects.all()]
-
-purposes = [(purpose.id, purpose.name) for purpose in Purpose.objects.all()]
+    purposes = [(purpose.id, purpose.name) for purpose in Purpose.objects.all()]
+except OperationalError:
+    print("Models have not been migrated yet, please migrate them before use.")
+    (boards, rigors, purposes) = ([],[],[])
+    
 
 class NewThreadForm(forms.Form):
     board_id = forms.ChoiceField(label="Board",
